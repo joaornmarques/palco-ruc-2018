3b5a7cd9c6549cb8bc6bc6a454214dc35d84b0f4
joaornmarques <joaornmarques@gmail.com>
2018-04-27 11:46:29 +0100

    artists links, images and animations

--- src/scripts/helpers/init.js
+++ src/scripts/helpers/init.js
@@ -1,19 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(){
-	console.log('tan tan tan')
+	console.log('site credits !INCLUDE DONT FORGET')
+});
+
+$(document).ready(function() {
+
+    $('.c-logo').click(function(){
+        $('html, body').animate({scrollTop:$(document).height()}, 'slow', 'swing');
+        return false;
+    });
+
 });
 
 $('.o-block').click(function() {
 	blockOverlay = $(this).children('.o-block__content--overlay');
 	blockBase = $(this).children('.o-block__content--base');
+	blockLabel = $(this).children('.c-shelf-label');
 	if(!blockOverlay.hasClass('o-block__content--overlay-show')){
 		//reset state
 		$('.o-block__content--base').removeClass('o-block__content--base-hide');
 		$('.o-block__content--overlay').removeClass('o-block__content--overlay-show');
+		$('.c-shelf-label').removeClass('c-shelf-label--hidden');
 
 		blockOverlay.show();
 		setTimeout(function() {
 			blockOverlay.addClass('o-block__content--overlay-show');
 			blockBase.addClass('o-block__content--base-hide');
+			blockLabel.addClass('c-shelf-label--hidden');
 		}, 10);
 	}
 });
